2e32af76bea8077155e0d58b869e170c3575e34d
Aaron Tsui <aarontsui66@gmail.com>
2023-05-17 22:00:26 -0400

    5/17 sectorUpdates

--- WebScraper/sectorUpdates.py
+++ WebScraper/sectorUpdates.py
@@ -1,20 +1,34 @@
 from bs4 import BeautifulSoup as bs
 import requests as req
+import pandas as pd
 
+# list of all sector urls to scrape
+sectors = ["https://www.cnbc.com/technology/", "https://www.cnbc.com/health-and-science/", "https://www.cnbc.com/energy/", 
+"https://www.cnbc.com/aerospace-defense/", "https://www.cnbc.com/retail/", "https://www.cnbc.com/e-commerce/"]
 
-url = "https://site.financialmodelingprep.com/financial-summary/AAPL"
+# function to get headlines given a URL
+def scrape(webURL):
+	soup = bs(req.get(url).content, "html.parser")
+	headlines = []
+	for link in soup.find_all('a'):
+		l = link.string
+		if(str(type(l)) == "<class 'bs4.element.NavigableString'>" and len(l.strip()) > 35 and 
+			not(l.strip().split()[0] == "Best" or l.strip().split()[0] == "Personal" or "Market Data Terms" in l)): #filtering out irrelevant navigable strings
+				headlines.append(l.strip())
+	return headlines
 
-webpage = req.get(url)
-trav = bs(webpage.content, "html.parser")
-M = 1
-for link in trav.find_all('a'):
+# dataframe w/ columns as URL(sector) and rows as headlines
+df = pd.DataFrame()
 
-	# PASTE THE CLASS TYPE THAT WE GET
-	# FROM THE ABOVE CODE IN THIS AND
-	# SET THE LIMIT GREATER THAN 35
-	if(str(type(link.string)) == "<class 'bs4.element.NavigableString'>"
-	and len(link.string) > 35):
+for url in sectors:
+	print("\n" + str(url) + "\n")
+	temp = pd.DataFrame({url:scrape(url)})
+	df = pd.concat([df, temp], ignore_index=False, axis=1)
+	for h in scrape(url):
+		print(h)
 
-		print(str(M)+".", link.string)
-		M += 1
-print(":asd")
+print(df)
+
+# export the dataframe to an Excel sheet
+with pd.ExcelWriter('/Users/aarontsui/Desktop/ORBS/sectorUpdates.xlsx', engine='openpyxl', mode='w') as writer:
+    df.to_excel(writer, sheet_name='Sheet1', index=False)
